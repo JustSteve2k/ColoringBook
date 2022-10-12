c6a7324d5cdb9ed38b099f24f9c8bbdf69e4f486
JustSteve2k <srodriguez2k@gmail.com>
2022-10-11 20:39:17 -0500

    add dynamic polygon and square functions

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ elements.forEach((element) => {
 
 const boxes = document.querySelectorAll(".colorBox");
 
-console.log(boxes);
+// console.log(boxes);
 
 boxes.forEach((element) => {
   element.addEventListener("click", (e) => {
@@ -90,6 +90,159 @@ function TranslateColor(selection) {
     case "box5":
       return "color5";
   }
+}
+
+function AddNewPolygon() {
+  // alert("add new polygon");
+  let polygon = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
+  polygon.setAttribute("class", "zone color1");
+  polygon.setAttribute("points", "525,25 600,25 600,100 525,100");
+  polygon.setAttribute("fill", "#000000");
+  let drawing = document.getElementById("drawing");
+  drawing.append(polygon);
+}
+
+function AddDynamicPolygon(points) {
+  // alert("add new polygon");
+  let pointsX = [];
+  let pointsY = [];
+
+  let MaxX = 650;
+  let MaxY = 650;
+
+  for (let x = 0; x < points; x++) {
+    pointsX[x] = Math.floor(Math.random() * MaxX);
+  }
+
+  for (let x = 0; x < points; x++) {
+    pointsY[x] = Math.floor(Math.random() * MaxY);
+  }
+
+  console.log(pointsX);
+  console.log(pointsY);
+
+  let pointsString = "";
+
+  for (let x = 0; x < points; x++) {
+    pointsString += pointsX[x].toString();
+    pointsString += ",";
+    pointsString += pointsY[x].toString();
+    pointsString += " ";
+  }
+
+  console.log(`The generated string is - ${pointsString}`);
+
+  let polygon = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
+
+  polygon.setAttribute("class", "zone color0");
+  polygon.setAttribute("points", pointsString);
+  polygon.setAttribute("fill", "currentcolor");
+
+  polygon.addEventListener("click", (e) => {
+    PaintItem(e);
+  });
+
+  // element.addEventListener("click", (e) => {
+  //   PaintItem(e);
+  // });
+
+  let drawing = document.getElementById("drawing");
+
+  drawing.append(polygon);
+}
+
+function AddSquare() {
+  // alert("add new polygon");
+  let polygon = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
+  polygon.setAttribute("class", "zone color1");
+  polygon.setAttribute("points", "525,25 600,25 600,100 525,100");
+  polygon.setAttribute("fill", "#000000");
+  let drawing = document.getElementById("drawing");
+  drawing.append(polygon);
+}
+
+function AddSquare() {
+  // alert("add new polygon");
+
+  let MaxX = 540;
+  let MaxY = 500;
+
+  let pointsX = [25, 100, 100, 25];
+  let pointsY = [25, 25, 100, 100];
+
+  let adjustX = Math.floor(Math.random() * MaxX);
+  let adjustY = Math.floor(Math.random() * MaxY);
+
+  let pointsString = "";
+
+  for (let x = 0; x < 4; x++) {
+    pointsString += (pointsX[x] + adjustX).toString();
+    pointsString += ",";
+    pointsString += (pointsY[x] + adjustY).toString();
+    pointsString += " ";
+  }
+
+  console.log(`The generated string is - ${pointsString}`);
+
+  let polygon = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
+
+  polygon.setAttribute("class", "zone color0");
+  polygon.setAttribute("points", pointsString);
+  polygon.setAttribute("fill", "currentcolor");
+
+  polygon.addEventListener("click", (e) => {
+    PaintItem(e);
+  });
+
+  // element.addEventListener("click", (e) => {
+  //   PaintItem(e);
+  // });
+
+  let drawing = document.getElementById("drawing");
+
+  drawing.append(polygon);
+}
+
+function AddTriangle() {
+  // alert("add new polygon");
+
+  let MaxX = 540;
+  let MaxY = 500;
+
+  let pointsX = [50, 0, 100];
+  let pointsY = [0, 100, 100];
+
+  let adjustX = Math.floor(Math.random() * MaxX);
+  let adjustY = Math.floor(Math.random() * MaxY);
+
+  let pointsString = "";
+
+  for (let x = 0; x < 3; x++) {
+    pointsString += (pointsX[x] + adjustX).toString();
+    pointsString += ",";
+    pointsString += (pointsY[x] + adjustY).toString();
+    pointsString += " ";
+  }
+
+  console.log(`The generated string is - ${pointsString}`);
+
+  let polygon = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
+
+  polygon.setAttribute("class", "zone color0");
+  polygon.setAttribute("points", pointsString);
+  polygon.setAttribute("fill", "currentcolor");
+
+  polygon.addEventListener("click", (e) => {
+    PaintItem(e);
+  });
+
+  // element.addEventListener("click", (e) => {
+  //   PaintItem(e);
+  // });
+
+  let drawing = document.getElementById("drawing");
+
+  drawing.append(polygon);
 }
 
 // console.log(elements);

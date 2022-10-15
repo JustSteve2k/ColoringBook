877cb1ad5d15e5891084c7f320f40b28ba739470
JustSteve2k <srodriguez2k@gmail.com>
2022-10-14 21:08:58 -0500

    add additional tests to generate array

--- public/js/shapes.test.js
+++ public/js/shapes.test.js
@@ -1,16 +1,21 @@
-import { AddTriangle, GeneratePointsString } from "./shapes.js";
-
-let pointsX = [50, 0, 100];
-let pointsY = [0, 100, 100];
+import { GeneratePointsArray, GeneratePointsStringFromArray } from "./shapes.js";
 
 test("generates string 1", () => {
-  expect(GeneratePointsString([50, 0, 100], [0, 100, 100])).toBe("50,0 0,100 100,100");
+  expect(GeneratePointsStringFromArray([50, 0, 100], [0, 100, 100])).toBe("50,0 0,100 100,100");
 });
 
 test("generates string 2", () => {
-  expect(GeneratePointsString([50, 0, 100, 20], [0, 100, 100, 50])).toBe("50,0 0,100 100,100 20,50");
+  expect(GeneratePointsStringFromArray([50, 0, 100, 20], [0, 100, 100, 50])).toBe("50,0 0,100 100,100 20,50");
 });
 
 test("generates string 3 - short string", () => {
-  expect(GeneratePointsString([50], [0])).toBe("50,0");
+  expect(GeneratePointsStringFromArray([50], [0])).toBe("50,0");
+});
+
+test("generates points array of 3 ints", () => {
+  expect(GeneratePointsArray(3, 50)).toHaveLength(3);
+});
+
+test("generates points array of 5 ints", () => {
+  expect(GeneratePointsArray(5, 50)).toHaveLength(5);
 });

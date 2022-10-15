d4f9ce184e636d6363544dfbfc9433214e8f69bc
JustSteve2k <srodriguez2k@gmail.com>
2022-10-14 21:09:31 -0500

    refactor functions on array generation

--- public/js/shapes.js
+++ public/js/shapes.js
@@ -1,31 +1,16 @@
 import { GetRandomInt } from "./helpers.js";
 
 export function AddDynamicPolygon(points) {
-  let pointsX = [];
-  let pointsY = [];
-
   let MaxX = 650;
   let MaxY = 650;
 
-  for (let x = 0; x < points; x++) {
-    pointsX[x] = Math.floor(Math.random() * MaxX);
-  }
-
-  for (let x = 0; x < points; x++) {
-    pointsY[x] = Math.floor(Math.random() * MaxY);
-  }
+  let pointsX = GeneratePointsArray(points, MaxX);
+  let pointsY = GeneratePointsArray(points, MaxY);
 
   console.log(pointsX);
   console.log(pointsY);
 
-  let pointsString = "";
-
-  for (let x = 0; x < points; x++) {
-    pointsString += pointsX[x].toString();
-    pointsString += ",";
-    pointsString += pointsY[x].toString();
-    pointsString += " ";
-  }
+  let pointsString = GeneratePointsStringFromArray(pointsX, pointsY);
 
   CreatePolygon(pointsString);
 }
@@ -61,17 +46,11 @@ export function AddTriangle() {
   let pointsX = [50, 0, 100];
   let pointsY = [0, 100, 100];
 
-  let adjustX = Math.floor(Math.random() * MaxX);
-  let adjustY = Math.floor(Math.random() * MaxY);
+  let adjustX = GetRandomInt(MaxX);
+  let adjustY = GetRandomInt(MaxY);
 
-  let pointsString = "";
+  let pointsString = GeneratePointsStringFromArray(pointsX, pointsY);
 
-  for (let x = 0; x < 3; x++) {
-    pointsString += (pointsX[x] + adjustX).toString();
-    pointsString += ",";
-    pointsString += (pointsY[x] + adjustY).toString();
-    pointsString += " ";
-  }
   CreatePolygon(pointsString);
 }
 
@@ -96,7 +75,16 @@ function CreatePolygon(pointsString) {
   drawing.append(polygon);
 }
 
-export function GeneratePointsString(pointsX, pointsY) {
+export function GeneratePointsArray(length, max, adjust = 1) {
+  let points = [];
+
+  for (let x = 0; x < length; x++) {
+    points[x] = GetRandomInt(max) * adjust;
+  }
+  return points;
+}
+
+export function GeneratePointsStringFromArray(pointsX, pointsY) {
   let pointsString = "";
 
   for (let x = 0; x < pointsX.length; x++) {

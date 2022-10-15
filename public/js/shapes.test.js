10638d287f5d3529ad195a853fc788713df35e33
JustSteve2k <srodriguez2k@gmail.com>
2022-10-15 17:46:20 -0500

    add new tests for adjustPointsOnPointsArray

--- public/js/shapes.test.js
+++ public/js/shapes.test.js
@@ -1,4 +1,4 @@
-import { GeneratePointsArray, GeneratePointsStringFromArray } from "./shapes.js";
+import { AdjustPointsOnPointsArray, AdjustPointsOnPointsStinrg, GeneratePointsArray, GeneratePointsStringFromArray } from "./shapes.js";
 
 test("generates string 1", () => {
   expect(GeneratePointsStringFromArray([50, 0, 100], [0, 100, 100])).toBe("50,0 0,100 100,100");
@@ -18,4 +18,12 @@ test("generates points array of 3 ints", () => {
 
 test("generates points array of 5 ints", () => {
   expect(GeneratePointsArray(5, 50)).toHaveLength(5);
+});
+
+test("adjusts a 3 point array by 20", () => {
+  expect(AdjustPointsOnPointsArray([20, 40, 60], 20)).toStrictEqual([40, 60, 80]);
+});
+
+test("adjusts a 4 point array by 10", () => {
+  expect(AdjustPointsOnPointsArray([10, 20, 30, 40], 10)).toStrictEqual([20, 30, 40, 50]);
 });

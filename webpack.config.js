a8382ee79bc84023f900679a7449c4b168247c2a
JustSteve2k <srodriguez2k@gmail.com>
2023-01-14 23:58:19 -0600

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 
 module.exports = {
   entry: "./public/js/script.js",
+  mode: "development",
   output: {
     filename: "script.js",
     path: path.resolve(__dirname, "dist"),
